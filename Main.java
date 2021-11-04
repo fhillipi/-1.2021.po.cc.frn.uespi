324620a74f9c93bb0a5b3600dd30c474c25b0abb
Fhillipi Silva <fhillipi.ti@gmail.com>
2021-11-04 20:16:32 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    Pessoal p = new Pessoal();
+    p.set_idade(31);
+
+    p.
+
+    System.out.println("A instância tem "+ p.get_idade() +" de idade");
+  }
+}
---
+++ Pessoa.java
@@ -0,0 +1,15 @@
+class Pessoal {
+  
+  private int idade;
+  protected int cpf;
+
+  public void set_idade(int idade) {
+    this.idade = idade;      
+  }
+
+  public int get_idade()
+  {
+    return(this.idade); 
+  }
+
+}
